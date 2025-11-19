76930ed0a68dc4138d76207b87b67fa75f982451
ibabdulaziz2197 <ibabdulaziz2197@gmail.com>
2025-11-19 15:08:37 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,64 @@
+# 1
+s = 'python2025'
+print(s)
+
+toza = s[0:-4]
+print(toza)
+
+# 2
+a = 'HelloWorldWideWeb'
+print(a)
+
+t = a[5:10]
+print(t)
+
+
+# 4
+s = "123abc456def789"
+print(s)
+
+t = s[::-1]
+print(t)
+
+# 5
+s = "ProgramingFun"
+print(s)
+
+t = s[-1:3]
+print(t)
+
+# 6
+s = "DataScience2025"
+print(s)
+
+t = s[4:11]
+print(t)
+
+# 7
+s = "abdefghij"
+print(s)
+
+t = s[2:8:2] + s[7:1:-1]
+print(t)
+
+# 8
+s = 'Xursandchilk'
+print(s)
+
+t = s[3:7]
+print(t)
+
+
+# 9
+s = "Al_Revalotion_2030"
+print(s)
+
+t = s[3:13]
+print(t)
+
+# 10
+s = "qwertyuiop"
+print(s)
+
+t = s[0:11:2]
+print(t)
